23a1406b85f0bc605d4438a427b52ac610fdb6e6
NGUYEN Tien Dung <ntd94@ubuntu>
2021-01-29 12:51:40 -0800

    new files and folders

---
+++ codingGame/2D_path_pb.C
@@ -0,0 +1,33 @@
+/*
+You are in a 2D array world with R rows and C columns, where you live in the (0,0) coordinate and you need to find the number of paths to the heaven cell (Rth row and Cth column).
+
+You always start from the top-left cell and try to reach the bottom-right cell.
+
+You can move either to the right or down one cell at a time.
+
+Cells marked as 0 can be moved to, while cells marked as 1 cannot be visited.
+
+Note: There might be zero paths to the heaven cell sometimes.
+Input
+Line 1: Two space-separated integers R and C
+Next R lines: C space-separated integers, being that each of those integers can only have the values 0 or 1
+Output
+The number of paths that you can reach the heaven cell.
+Constraints
+1 ≤ R, C ≤ 6
+Example
+Input
+3 3
+0 0 0
+0 1 0
+0 0 0
+Output
+2
+*/
+-
+-
+-
+-
+-
+-
+-
